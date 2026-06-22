7e22bd25e51b3708c485044927e5609a04757e1d
ShoveetSingh <shoveetsingh2002@gmail.com>
2026-06-22 23:42:08 +0530

    Sentence Transformers

--- app.py
+++ app.py
@@ -1,2 +1,35 @@
 from flask import Flask
-from pandas import pd
+import pandas as  pd
+from sentence_transformers import SentenceTransformer,util
+from docx import Document
+
+
+model=SentenceTransformer('all-MiniLM-L6-v2')
+
+data = Document('datasets/job_description.docx')
+
+job_data=''
+
+for p in data.paragraphs:
+    job_data+=p.text+' '
+
+emb1 = model.encode(job_data)
+
+candidate_data=pd.read_json('datasets/candidates.jsonl',lines=True)
+
+list=[]
+for row in candidate_data.itertuples(index=True):
+
+    s=''
+    for item in row:
+        s+=str(item)+' '
+    emb2 = model.encode(s)
+    similarity = util.cos_sim(emb1,emb2).item()
+    if similarity >= 0.5:
+        list.append(similarity)
+
+candidate_data['Similarity']=list
+
+df = candidate_data.sort_values(by='Similarity',ascending=False)
+
+print(df.head(100))
